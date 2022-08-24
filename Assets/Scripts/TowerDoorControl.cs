fc0d938035bd31e59c211d69adcb67759d828be1
tomix89 <84495269+tomix89@users.noreply.github.com>
2022-08-24 11:24:25 +0200

    Update
    - added a few more sounds
    - proper end for the door animation (checking for animation to end)

--- Assets/Scripts/TowerDoorControl.cs
+++ Assets/Scripts/TowerDoorControl.cs
@@ -6,24 +6,40 @@ public class TowerDoorControl : MonoBehaviour
 {
     [SerializeField] private GameObject m_key;
     private bool openingAlreadyStarted = false;
+    private Animator animator;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    bool AnimatorIsPlaying() {
+        return animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.name == "Player") {
+            // check if key was collected, and the animation was not started already
             if (!m_key.activeSelf && !openingAlreadyStarted) {
                 openingAlreadyStarted = true;
                 GetComponent<AudioSource>().Play();
-                GetComponent<Animator>().Play("tower_door_opens");
-                StartCoroutine(Deactivate(2.5f));
+                animator.Play("tower_door_opens");
+
+                // start polling for animation ended
+                StartCoroutine(Deactivate(0.2f));
             }
         }
     }
 
-    // make the apple disappear a bit later to have a better audio / video sync
+    // make the door disappear a bit later to be able to play the sound and animation
     IEnumerator Deactivate(float time) {
         yield return new WaitForSeconds(time);
         // Code to execute after the delay
 
-        this.gameObject.SetActive(false);
+        if (AnimatorIsPlaying()) {
+            StartCoroutine(Deactivate(0.2f));
+        } else {
+            this.gameObject.SetActive(false);
+        }
     }
 
 }
